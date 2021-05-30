d3a3762183edc73d8dfd9791ab660ca8a88b8b15
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-05-30 01:10:09 -0300

    refactor: added projectPath variable

--- src/WarehouseSimulation.cpp
+++ src/WarehouseSimulation.cpp
@@ -41,7 +41,7 @@ void SigintHandler(int sig)
     isShutdown = true;
 }
 
-// Build and return a geometry_msgs::Pose from string values
+// Build and return a geometry_msgs::Pose from string values of xyzrpy
 geometry_msgs::Pose GetPoseFromString(float x, float y, float z, float roll, float pitch, float yaw)
 {
     geometry_msgs::Pose pose;
@@ -63,22 +63,22 @@ geometry_msgs::Pose GetPoseFromString(float x, float y, float z, float roll, flo
 }
 
 // Load all specified models to the model controller
-void LoadModels(std::shared_ptr<ModelController> &modelController)
+void LoadModels(std::shared_ptr<ModelController> &modelController, std::string projectDirectory)
 {
-    modelController->Add("ProductR", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/ProductR.sdf");
-    modelController->Add("ProductG", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/ProductG.sdf");
-    modelController->Add("ProductB", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/ProductB.sdf");
+    modelController->Add("ProductR", projectDirectory + "/models/warehouseObjects/ProductR.sdf");
+    modelController->Add("ProductG", projectDirectory + "/models/warehouseObjects/ProductG.sdf");
+    modelController->Add("ProductB", projectDirectory + "/models/warehouseObjects/ProductB.sdf");
 
-    modelController->Add("StorageR", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/StorageR.sdf");
-    modelController->Add("StorageG", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/StorageG.sdf");
-    modelController->Add("StorageB", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/StorageB.sdf");
+    modelController->Add("StorageR", projectDirectory + "/models/warehouseObjects/StorageR.sdf");
+    modelController->Add("StorageG", projectDirectory + "/models/warehouseObjects/StorageG.sdf");
+    modelController->Add("StorageB", projectDirectory + "/models/warehouseObjects/StorageB.sdf");
 
-    modelController->Add("DispatchA", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/Dispatch.sdf");
-    modelController->Add("DispatchB", "/home/renato/catkin_ws/src/delivery_robot_simulation/models/warehouseObjects/Dispatch.sdf");
+    modelController->Add("DispatchA", projectDirectory + "/models/warehouseObjects/Dispatch.sdf");
+    modelController->Add("DispatchB", projectDirectory + "/models/warehouseObjects/Dispatch.sdf");
 }
 
 // Read the configuration files and instantiate the warehouse objects
-bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages, std::vector<std::shared_ptr<Dispatch>> &dispatches, std::shared_ptr<ModelController> modelController, std::string configsDirectory)
+bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages, std::vector<std::shared_ptr<Dispatch>> &dispatches, std::shared_ptr<ModelController> modelController, std::string projectDirectory)
 {
     std::string line;
 
@@ -86,7 +86,7 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
     try
     {
         // Read Storages configuration file
-        std::ifstream storagesConfigFs(configsDirectory + "storages");
+        std::ifstream storagesConfigFs(projectDirectory + "/configs/storages");
         if (storagesConfigFs.is_open())
         {
             while (std::getline(storagesConfigFs, line))
@@ -114,6 +114,7 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
                 std::string opx{};
                 std::string opy{};
                 std::string opz{};
+                std::string maxCapacity{};
 
                 // Get the Storage model name
                 linestream >> storageModel;
@@ -121,16 +122,19 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
                 // Get the Storage's Product name that will be used in Production
                 linestream >> productionModel;
 
+                // Get the Storage's max capacity of stored Product
+                linestream >> maxCapacity;
+
                 // Get the X, Y and Z position and orientation coordinates of the Storage
                 linestream >> psx >> psy >> psz >> osx >> osy >> osz;
-                storagePose = GetPoseFromString(std::stof(psx),std::stof(psy),std::stof(psz),std::stof(osx),std::stof(osy),std::stof(osz));
+                storagePose = GetPoseFromString(std::stof(psx), std::stof(psy), std::stof(psz), std::stof(osx), std::stof(osy), std::stof(osz));
 
                 // Get the X, Y and Z position and orientation coordinates where the produced Products will be spawned when requested
                 linestream >> ppx >> ppy >> ppz >> opx >> opy >> opz;
-                productOutputPose = GetPoseFromString(std::stof(ppx),std::stof(ppy),std::stof(ppz),std::stof(opx),std::stof(opy),std::stof(opz));
+                productOutputPose = GetPoseFromString(std::stof(ppx), std::stof(ppy), std::stof(ppz), std::stof(opx), std::stof(opy), std::stof(opz));
 
                 // Add a new Storage to the Storages Vector
-                storages.push_back(std::make_shared<Storage>(storageModel, productionModel, storagePose, productOutputPose, modelController));
+                storages.push_back(std::make_shared<Storage>(storageModel, productionModel, std::stoi(maxCapacity), storagePose, productOutputPose, modelController));
             }
         }
         storagesConfigFs.close();
@@ -140,7 +144,7 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
     {
         // Convert exception to a readable string and print a message
         std::ostringstream s;
-        s << e.what() ;
+        s << e.what();
         Print("Error while instantiating a Dispatch: " + s.str());
         return false;
     }
@@ -149,7 +153,7 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
     try
     {
         // Read Dispatches configuration file
-        std::ifstream dispatchesConfigFs(configsDirectory + "dispatches");
+        std::ifstream dispatchesConfigFs(projectDirectory + "/configs/dispatches");
         if (dispatchesConfigFs.is_open())
         {
             while (std::getline(dispatchesConfigFs, line))
@@ -182,12 +186,12 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
 
                 // Get the X, Y and Z position and orientation coordinates of the Dispatch
                 linestream >> pdx >> pdy >> pdz >> odx >> ody >> odz;
-                dispatchPose = GetPoseFromString(std::stof(pdx),std::stof(pdy),std::stof(pdz),std::stof(odx),std::stof(ody),std::stof(odz));
+                dispatchPose = GetPoseFromString(std::stof(pdx), std::stof(pdy), std::stof(pdz), std::stof(odx), std::stof(ody), std::stof(odz));
 
                 // Get the X, Y and Z position and orientation coordinates where the produced Products will be spawned when requested
                 linestream >> ppx >> ppy >> ppz >> opx >> opy >> opz;
-                productPickPose = GetPoseFromString(std::stof(ppx),std::stof(ppy),std::stof(ppz),std::stof(opx),std::stof(opy),std::stof(opz));
-                
+                productPickPose = GetPoseFromString(std::stof(ppx), std::stof(ppy), std::stof(ppz), std::stof(opx), std::stof(opy), std::stof(opz));
+
                 // Add a new Dispatch to the Dispatches Vector
                 dispatches.push_back(std::make_shared<Dispatch>(dispatchModel, dispatchPose, productPickPose, modelController));
             }
@@ -199,15 +203,17 @@ bool InstatiateWarehouseObjects(std::vector<std::shared_ptr<Storage>> &storages,
     {
         // Convert exception to a readable string and print a message
         std::ostringstream s;
-        s << e.what() ;
+        s << e.what();
         Print("Error while instantiating a Dispatch: " + s.str());
         return false;
     }
 
     Print("Storages created: " + std::to_string(storages.size()));
     Print("Dispatches created: " + std::to_string(dispatches.size()));
+
     // If both Storages and Dispatches were instantiated properly, then return true
-    return true;
+    // If storages and/or dispaches are empty, then return false
+    return (storages.size() > 0 && dispatches.size() > 0) ? true : false;
 }
 
 int main(int argc, char **argv)
@@ -217,8 +223,8 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "WarehouseSimulation", ros::init_options::NoSigintHandler);
     ros::NodeHandle n;
 
-    // Set the configuration directory
-    std::string configsDirectory{"/home/renato/catkin_ws/src/delivery_robot_simulation/configs/"};
+    // Set the project directory
+    std::string projectDirectory{"../catkin_ws/src/warehouse_robot_simulation"};
 
     // Create an instance of the modelController
     std::shared_ptr<ModelController> modelController = std::make_shared<ModelController>("ModelController");
@@ -232,11 +238,11 @@ int main(int argc, char **argv)
     std::vector<std::shared_ptr<Robot>> robots;
 
     // Load models
-    LoadModels(modelController);
+    LoadModels(modelController, projectDirectory);
 
     // Instantiate Storage and Dispatch objects from config file
     // If any exception happened, end this simulation
-    bool readConfigFiles = InstatiateWarehouseObjects(storages, dispatches, modelController, configsDirectory);
+    bool readConfigFiles = InstatiateWarehouseObjects(storages, dispatches, modelController, projectDirectory);
     if (!readConfigFiles)
     {
         Print("Please, check your configuration files and try again");
@@ -249,20 +255,19 @@ int main(int argc, char **argv)
     robots.emplace_back(std::make_shared<Robot>("amr", "move_base", storages, dispatches, orderController));
 
     // Spawn each Storage in simulation and start it's operation
-    std::for_each(storages.begin(), storages.end(), [modelController](std::shared_ptr<Storage> &s) {
-        modelController->Spawn(s->GetName(), s->GetModelName(), s->GetPose());
-        s->StartOperation();
-    });
+    std::for_each(storages.begin(), storages.end(), [modelController](std::shared_ptr<Storage> &s)
+                  {
+                      modelController->Spawn(s->GetName(), s->GetModelName(), s->GetPose());
+                      s->StartOperation();
+                  });
 
     // Spawn each Dispatch in simulation
-    std::for_each(dispatches.begin(), dispatches.end(), [modelController](std::shared_ptr<Dispatch> &d) {
-        modelController->Spawn(d->GetName(), d->GetModelName(), d->GetPose());
-    });
+    std::for_each(dispatches.begin(), dispatches.end(), [modelController](std::shared_ptr<Dispatch> &d)
+                  { modelController->Spawn(d->GetName(), d->GetModelName(), d->GetPose()); });
 
     // Start each Robots operation
-    std::for_each(robots.begin(), robots.end(), [modelController](std::shared_ptr<Robot> &r) {
-        r->StartOperation();
-    });
+    std::for_each(robots.begin(), robots.end(), [modelController](std::shared_ptr<Robot> &r)
+                  { r->StartOperation(); });
 
     ros::Subscriber ordersSubscriber = n.subscribe("warehouse/order/add", 10, &OrderController::AddOrder, &*orderController);
 
@@ -278,26 +283,25 @@ int main(int argc, char **argv)
     }
 
     // Start the shutting down process by deleting each spawned object from simulation
-    std::for_each(storages.begin(), storages.end(), [modelController](std::shared_ptr<Storage> &s) {
-        modelController->Delete(s->GetName());
-    });
+    std::for_each(storages.begin(), storages.end(), [modelController](std::shared_ptr<Storage> &s)
+                  { modelController->Delete(s->GetName()); });
 
-    std::for_each(dispatches.begin(), dispatches.end(), [modelController](std::shared_ptr<Dispatch> &d) {
-        modelController->Delete(d->GetName());
-    });
+    std::for_each(dispatches.begin(), dispatches.end(), [modelController](std::shared_ptr<Dispatch> &d)
+                  { modelController->Delete(d->GetName()); });
 
     // Check if there is any Product in the robots and delete it
-    std::for_each(robots.begin(), robots.end(), [modelController](std::shared_ptr<Robot> &r) {
-        std::vector<std::string> productsName = r->GetCargoBinProductsName();
-        if (productsName.size() > 0)
-        {
-            // Iterate over productsName and request modelController Delete
-            for (auto &p : productsName)
-            {
-                modelController->Delete(p);
-            }
-        }
-    });
+    std::for_each(robots.begin(), robots.end(), [modelController](std::shared_ptr<Robot> &r)
+                  {
+                      std::vector<std::string> productsName = r->GetCargoBinProductsName();
+                      if (productsName.size() > 0)
+                      {
+                          // Iterate over productsName and request modelController Delete
+                          for (auto &p : productsName)
+                          {
+                              modelController->Delete(p);
+                          }
+                      }
+                  });
 
     // Wait a bit more to guarantee the modelController delete requests be done
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
